4ca7a2896a24486f12e1b72a20b53957c183d35b
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-09-26 11:03:14 +0100

    Split out models

--- app/server.rb
+++ app/server.rb
@@ -7,9 +7,18 @@ require 'sinatra/partial'
 
 class Server < Sinatra::Base
 
-  require './lib/link'
-  require './lib/tag'
-  require './lib/user'
+require_relative 'models/link'
+require_relative 'models/tag'
+require_relative 'models/user'
+require_relative 'helpers/application'
+require_relative 'data_mapper_setup'
+
+# require_relative 'controllers/users'
+# require_relative 'controllers/sessions'
+# require_relative 'controllers/links'
+# require_relative 'controllers/tags'
+# require_relative 'controllers/application'
+
   require_relative 'helpers/application'
   require_relative 'data_mapper_setup'
 
